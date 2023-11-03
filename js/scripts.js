e64687c292d121a88b6f6faa1f505c16923028f9
Jonathan Song <jonathan91790@yahoo.com>
2023-11-03 14:52:56 -0700

    finish topping display WIP price

--- js/scripts.js
+++ js/scripts.js
@@ -18,24 +18,36 @@ Pizza.prototype.determinePrice = function() {
     }
 }
 
-function handleSubmit() {
+function handleSubmit(e) {
+    e.preventDefault()
     let toppings = []
+    const pizzaDisplay = document.querySelector(".pizzaDisplay")
+    const pizzaToppings = document.createElement('ul')
     const checked = document.querySelectorAll('input[type="checkbox"]:checked')
     toppings = Array.from(checked).map(topping => topping.value)
     const size = document.querySelector("#size").value
     const pizza = new Pizza(toppings,size)
+    displayPizza(pizza)
 
 
 }
 
 function displayPizza(pizza) {
-    let toppingList = document.querySelector
+    const pizzaDisplay = document.querySelector(".pizza-display")
+    const pizzaTitle = document.createElement("h3")
+    pizzaTitle.append("Pizza")
+    pizzaDisplay.append(pizzaTitle)
+    const toppingsTitle = document.createElement("h4")
+    toppingsTitle.append("Toppings:")
+    pizzaDisplay.append(toppingsTitle)
+    const toppingList = document.createElement('ul')
     pizza.toppings.forEach(topping => {
-        text += " " + topping
+        let listItem = document.createElement("li")
+        listItem.append(topping)
+        toppingList.append(listItem)
     })
-    document.querySelector(".pizza-type")
-
-
+    pizzaDisplay.append(toppingList)
+    
 }
 
 
